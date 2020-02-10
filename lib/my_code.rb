76b46d31b7af4fcdae227ad4d91ca5cf32361d2f
Skye Geraghty <skyetay03@googlemail.com>
2020-02-10 07:19:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+def map(source)
+  output = []
+  index = 0
+  
+  while index < source.length do
+    output.push(yield(source[index]))
+    index += 1
+  end
+  return output
+end
+
+def reduce(source, output = 0)
+  index = 0
+
+  while index < source.length do
+    
+    output = yield(source[index], output)
+    
+    index += 1
+  end
+  # I feel really badly about the following line of Code
+  # I intend to revisit this when my knowledge is better and correct it
+  if output == nil
+    output = false
+  end
+  output
+end
