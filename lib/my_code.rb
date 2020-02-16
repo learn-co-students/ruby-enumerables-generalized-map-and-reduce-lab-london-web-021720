fe9da62375082e2fd9cb48262f47289b619f0aed
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-02-16 13:09:49 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+def map (array)
+new_array = []
+i = 0
+while i < array.length do
+
+new_array << yield(array[i])
+i +=1
+end
+
+  return new_array
+
+
+end
+
+
+def reduce(array, extra = nil )
+if extra
+   num1 = extra
+   i =0
+ else
+   num1 = array[0]
+   i=1
+ end
+while i < array.length
+  num1 = yield(num1, array[i])
+i+=1
+end
+return num1
+end
